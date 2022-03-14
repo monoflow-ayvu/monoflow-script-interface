c66cd6b947e1700d58c047e039f160f085f2be65
Fernando Mumbach <fermuch@ayvu.net>
2022-03-14 08:47:40 -0300

    applying custom config to device

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,40 @@
 import * as MonoUtils from "@fermuch/monoutils";
 
 // based on settingsSchema @ package.json
-type Config = Record<string, unknown> & {
-  nome: string;
+type Config = {
+  interfaceColor?: 'default' | 'dark' | 'light';
+  loginKeyboardType?:
+      'default'
+    | 'numeric'
+    | 'email-address'
+    | 'ascii-capable'
+    | 'numbers-and-punctuation'
+    | 'url'
+    | 'number-pad'
+    | 'phone-pad'
+    | 'name-phone-pad'
+    | 'decimal-pad'
+    | 'twitter'
+    | 'web-search'
+    | 'visible-password';
 }
 
 const conf = new MonoUtils.config.Config<Config>();
 
 messages.on('onInit', function() {
-  platform.log('script started! (I am the script)');
-  platform.log('settings:');
-  platform.log(conf.store);
+  platform.log('interface script started');
 
-  const name = conf.get('nome', 'default name');
-  platform.log(`Hello, ${name}!`);
+  const interfaceColor = conf.get('interfaceColor', 'default');
+  if (interfaceColor !== 'default') {
+    if (interfaceColor === 'dark') {
+      env.setData('SET_DARK_MODE', true);
+    } else {
+      env.setData('SET_DARK_MODE', false);
+    }
+  }
+
+  const loginKeyboardType = conf.get('loginKeyboardType', 'default');
+  if (loginKeyboardType !== 'default') {
+    env.setData('LOGIN_KEYBOARD_TYPE', loginKeyboardType);
+  }
 });
